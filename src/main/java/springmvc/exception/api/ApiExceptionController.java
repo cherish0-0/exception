c6ddc3987c1e3248a7ba784a433050a337149238
cherish0-0 <jiyu01160@naver.com>
2025-06-17 16:14:06 +0900

    feat(controller): API 예외 컨트롤러 구현

---
+++ src/main/java/springmvc/exception/api/ApiExceptionController.java
@@ -0,0 +1,34 @@
+package springmvc.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * API 예외 처리
+ * 단순 회원 조회, id가 "ex"인 경우 예외 발생
+ */
+@Slf4j
+@RestController
+public class ApiExceptionController {
+
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
+
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String id;
+        private String name;
+    }
+}
