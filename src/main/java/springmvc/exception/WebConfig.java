cb2a2bf4240eb6fccb24d2b9c7b1b6e3b3afdacc
cherish0-0 <jiyu01160@naver.com>
2025-06-17 14:56:50 +0900

    docs: 필터와 인터셉터가 중복 호출을 막는 방법에 대해 주석 추가

--- src/main/java/springmvc/exception/WebConfig.java
+++ src/main/java/springmvc/exception/WebConfig.java
@@ -20,6 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
         // dispatcherType 설정은 인터셉터에는 적용되지 않지만 excludePathPatterns를 통해 제어 가능
+        // 즉, 경로 정보로 중복 호출 제거 => WAS가 오류 페이지 확인 후 뷰에 렌더링할 때 인터셉터를 거치지 않음
     }
 
     // @Bean
@@ -28,6 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1); // 필터 순서 설정
         filterRegistrationBean.addUrlPatterns("/*"); // 필터 적용 URL 패턴 설정
+        // 필터는 dispatcherType=REQUEST로 중복 호출 제거 => WAS가 오류 페이지 확인 후 뷰에 렌더링할 때 필터를 거치지 않음
         filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
         return filterRegistrationBean;
     }
