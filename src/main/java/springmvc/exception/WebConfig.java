905607a4d147171c3a9685fa4bb5968e6cf91728
cherish0-0 <jiyu01160@naver.com>
2025-06-17 18:40:30 +0900

    feat(config): handlerExceptionResolver 사용할 수 있도록 config에 설정 등록

--- src/main/java/springmvc/exception/WebConfig.java
+++ src/main/java/springmvc/exception/WebConfig.java
@@ -5,10 +5,14 @@ import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springmvc.exception.filter.LogFilter;
 import springmvc.exception.interceptor.LogInterceptor;
+import springmvc.exception.resolver.MyHandlerExceptionResolver;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -21,6 +25,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
         // dispatcherType 설정은 인터셉터에는 적용되지 않지만 excludePathPatterns를 통해 제어 가능
         // 즉, 경로 정보로 중복 호출 제거 => WAS가 오류 페이지 확인 후 뷰에 렌더링할 때 인터셉터를 거치지 않음
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        // 예외 처리 핸들러 추가
+        resolvers.add(new MyHandlerExceptionResolver());
+        // 이 핸들러는 IllegalArgumentException을 400으로 처리하고, 다른 예외는 처리하지 않음
+        // 따라서, IllegalArgumentException이 발생하면 이 핸들러가 호출되어 400 상태 코드를 반환함
     }
 
     // @Bean
