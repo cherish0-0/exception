a79b9f28e4ae031a0749a3d74150cae9b997f9ab
cherish0-0 <jiyu01160@naver.com>
2025-06-17 12:32:15 +0900

    feat(controller): 예외를 발생시키지는 않고 response에 에러 메시지를 담아 서블릿 컨테이너에 보냄
    - 서블릿 컨테이너가 위임한 요청을 서블릿이 추후에 처리 -> 렌더링된 뷰를 통해 볼 수 있음

--- src/main/java/springmvc/exception/servlet/ServletExController.java
+++ src/main/java/springmvc/exception/servlet/ServletExController.java
@@ -1,8 +1,11 @@
 package springmvc.exception.servlet;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
 
 @Slf4j
 /**
@@ -19,5 +22,23 @@ public class ServletExController {
     @GetMapping("/error-ex")
     public void errorEx() {
         throw new RuntimeException("예외 발생!");
+    }
+
+    /**
+     * sendError 메서드로 HTTP 오류 상태 코드와 메시지를 클라이언트에게 전송
+     * 실제로 예외가 발생한 것은 아니고 가상의 상태를 전달함
+     * 전달받은 상태 코드는 WAS에서 오류로 인식됨
+     * 서블릿이 오류 처리를 해 에러 뷰 렌더링 됨
+     * @param response
+     * @throws IOException
+     */
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404 오류");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500);
     }
 }
